18d0e0e70e8b6998b6ec6537281d5447936aba4b
ben.huang52 <ben.huang@titansoft.com.sg>
2019-10-01 09:50:37 +0800

    create project

---
+++ CountingDuplicatesTest/CountingDuplicatesTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CountingDuplicatesTest
+{
+    [TestClass]
+    public class CountingDuplicatesTest
+    {
+        [TestMethod]
+        public void NoCharactersRepeat()
+        {
+            var stringService = new StringService();
+
+            var charCounting = stringService.CharCounting("question");
+
+            Assert.AreEqual(0, charCounting);
+        }
+    }
+
+    public class StringService
+    {
+        public int CharCounting(string question)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CountingDuplicatesTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CountingDuplicatesTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CountingDuplicatesTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e73af40c-ab08-44ef-8b58-d115081698ca")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
