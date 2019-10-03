e8763e7a4a8340d509f5a37a603cf374dd06afa9
ben.huang52 <ben.huang@titansoft.com.sg>
2019-10-04 00:28:18 +0800

    make by Linq

--- CountingDuplicatesTest/StringService.cs
+++ CountingDuplicatesTest/StringService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace CountingDuplicatesTest
 {
@@ -6,20 +7,7 @@ namespace CountingDuplicatesTest
     {
         public int CountDuplicateChar(string question)
         {
-            var duplicateChars = new List<char>();
-            var lowerStr = question.ToLower();
-            for (int i = 0; i < lowerStr.Length; i++)
-            {
-                for (int j = i + 1; j < lowerStr.Length; j++)
-                {
-                    if (lowerStr[j].Equals(lowerStr[i]) && !duplicateChars.Contains(lowerStr[j]))
-                    {
-                        duplicateChars.Add(lowerStr[j]);
-                    }
-                }
-            }
-
-            return duplicateChars.Count;
+            return question.GroupBy(c => c).Count(g => g.Count<char>() > 1);
         }
     }
 }
